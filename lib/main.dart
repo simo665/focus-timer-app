8f17ebef9a5c66f00bb73c834494b5e2e873725a
simo <jumpursimo12@gmail.com>
2026-02-03 20:49:29 +0100

    Pause/Resume logic implementation is finished. Disable buttons dynamically is finished.

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,7 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: TimerScreen(),
-    );
+    return const MaterialApp(home: TimerScreen());
   }
 }
 
@@ -27,18 +25,22 @@ class TimerScreen extends StatefulWidget {
 }
 
 class _TimerScreenState extends State<TimerScreen> {
-  // timer logic
-  Timer? _timer;
-  static const int defaultTime = 60*25;
+
+  static const int defaultBusyTime = 60*24;
   static const int defaultRestTime = 60*5;
-  int _secondsRemaining = defaultTime;
+  static const String ringTone1 = "sounds/cute_anime_ringtone.mp3";
+  static const String ringTone2 = "sounds/cute_alarm.mp3";
+
+  Timer? _timer;
+  int _secondsRemaining = defaultBusyTime;
   bool isPause = false;
-  String pauseText = "Pause";
-  
+  bool _isResting = false;
+  bool _isTimerStarted = false;
+
   // background colors
   Color _appBarColor = Color(0xffFFB3CB);
   Color _backgroundColor = Color(0xffDE648A);
-  
+
   // Ringtone player
   final AudioPlayer _player = AudioPlayer();
 
@@ -46,105 +48,93 @@ class _TimerScreenState extends State<TimerScreen> {
   void _changeColors(bool busy) {
     if (busy) {
       setState(() {
-      _backgroundColor = Color(0xff9F47DE);
-      _appBarColor = Color(0xffC795DE);
+        _backgroundColor = Color(0xff9F47DE);
+        _appBarColor = Color(0xffC795DE);
       });
     } else {
       _appBarColor = Color(0xffFFB3CB);
       _backgroundColor = Color(0xffDE648A);
     }
   }
-  
+
   // play ringtone function
   void _playRingTone(String source) async {
     await _player.play(AssetSource(source));
   }
-
   // starting timer button logic
-  void _startTimer(bool restTimer) {
+  void _startTimer(bool restTimer, {bool reset = true, bool isButton = false}) {
+    // Check if the user clicking starts while the timer already started
+    if (isButton & _isTimerStarted) {
+      return;
+    }
+
     // cancel previous timer if it exists
     _timer?.cancel();
     setState(() {
       isPause = false;
+      _isResting = restTimer;
+      _isTimerStarted = true;
     });
-
     // reset remaining seconds
-    setState(() {
-      if (!restTimer) {
-        _secondsRemaining = defaultTime;
-      } else {
-        _secondsRemaining = defaultRestTime;
-      }
-    });
-
-    // change background color
-    if (restTimer) {
-      _changeColors(false);
-    } else {
-      _changeColors(true);
+    if (reset) {
+      setState(() {
+        _secondsRemaining = restTimer ? defaultRestTime : defaultBusyTime;
+      });
     }
-
+    // change background color
+    _changeColors(!restTimer);
     // start new timer
-    _timer = Timer.periodic(
-      const Duration(seconds: 1),
-      (Timer timer) {
-        setState(() {
-          // check if timer is finished
-          if (_secondsRemaining == 0) {
-            // cancel timer
-            timer.cancel();
-            // change color
-            _changeColors(false);
-            // play ring tone for notification
-            if (restTimer) {
-              _playRingTone("sounds/cute_anime_ringtone.mp3");
-            } else {
-              _playRingTone("sounds/cute_alarm.mp3");
-            }
+    _timer = Timer.periodic(const Duration(seconds: 1), (Timer timer) {
+      setState(() {
+        // check if timer is finished
+        if (_secondsRemaining == 0) {
+          // cancel timer
+          timer.cancel();
+          // change color
+          _changeColors(false);
+          // play ring tone for notification
+          _playRingTone( restTimer ? ringTone2 : ringTone1);
 
-            // wait x seconds before next time
-            // start new timer for rest if not in rest already
-            Future.delayed(const Duration(seconds: 10), () {
-              if (!restTimer) {
-                _startTimer(true);
-              } else {
-                _startTimer(false);
-              }
-            });
-            return;
-          } else {
-            _secondsRemaining--;
-          }
-        });
-      }
-    );
+          // wait x seconds before next time
+          // start new timer for rest if not in rest already
+          Future.delayed(const Duration(seconds: 10), () {
+            _startTimer(!restTimer);
+          });
+          return;
+        } else {
+          _secondsRemaining--;
+        }
+      });
+    });
   }
 
   void _pauseTimer() {
-    if (isPause) {
-      setState(() {
-        isPause = false;
-        pauseText = "Pause";
+    if (!_isTimerStarted) {
+      return;
+    }
 
-      });
+    _timer?.cancel();
+    if (isPause) {
+      _startTimer(_isResting, reset: false);
     } else {
       setState(() {
         isPause = true;
-        pauseText = "Resume";
-        _timer?.cancel();
+        _changeColors(false);
       });
     }
-    setState(() {
-      _changeColors(false);
-    });
   }
 
   void _resetTimer() {
+    if (!_isTimerStarted) {
+      return;
+    }
+
     _timer?.cancel();
     setState(() {
-      isPause = true;
-      _secondsRemaining = defaultTime;
+      isPause = false;
+      _secondsRemaining = defaultBusyTime;
       _changeColors(false);
+      _isTimerStarted = false;
     });
   }
 
@@ -163,56 +153,76 @@ class _TimerScreenState extends State<TimerScreen> {
     return "$twoDigitMinutes:$twoDigitSeconds";
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Focus Time", style: TextStyle(
-            fontSize: 32,
-            fontFamily: "Knewave"
-          ),),
-          backgroundColor: _appBarColor,
+      appBar: AppBar(
+        title: const Text(
+          "Focus Time",
+          style: TextStyle(fontSize: 32, fontFamily: "Knewave"),
         ),
-        backgroundColor: _backgroundColor,
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(_formatDuration(_secondsRemaining), style: const TextStyle(fontSize: 80, fontFamily: "Knewave")),
-            const SizedBox(height: 20,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  onPressed: () => _startTimer(false),
-                  child: const Text("Start", style: TextStyle(
-                      fontSize: 24,
-                      fontFamily: "Knewave",
-                      color: Color(0xffDE5086)
-                  ),),
+        backgroundColor: _appBarColor,
+      ),
+      backgroundColor: _backgroundColor,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            _formatDuration(_secondsRemaining),
+            style: const TextStyle(fontSize: 80, fontFamily: "Knewave"),
+          ),
+          const SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () => _startTimer(false, isButton: true,),
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: _isTimerStarted ? Colors.white60 : Colors.white
                 ),
-                const SizedBox(width: 5,),
-                ElevatedButton(
-                  onPressed: _pauseTimer,
-                  child: Text(pauseText, style: TextStyle(
+                child: const Text(
+                  "Start",
+                  style: TextStyle(
                     fontSize: 24,
                     fontFamily: "Knewave",
-                    color: Color(0xffDE5086)
-                  ),),
+                    color: Color(0xffDE5086),
+                  ),
                 ),
-                const SizedBox(width: 5,),
-                ElevatedButton(
-                  onPressed: _resetTimer,
-                  child: const Text("Reset", style: TextStyle(
-                      fontSize: 24,
-                      fontFamily: "Knewave",
-                      color: Color(0xffDE5086)
-                  ),),
+              ),
+              const SizedBox(width: 5),
+              ElevatedButton(
+                onPressed: _pauseTimer,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: _isTimerStarted ? Colors.white : Colors.white60
                 ),
-              ]
-            )
-          ]
-        )
+                child: Text(
+                  isPause ? "Resume" : "Pause",
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontFamily: "Knewave",
+                    color: Color(0xffDE5086),
+                  ),
+                ),
+              ),
+              const SizedBox(width: 5),
+              ElevatedButton(
+                onPressed: _resetTimer,
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: _isTimerStarted ? Colors.white : Colors.white60
+                ),
+                child: const Text(
+                  "Reset",
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontFamily: "Knewave",
+                    color: Color(0xffDE5086),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
